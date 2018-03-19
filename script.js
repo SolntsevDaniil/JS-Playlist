2d77b7633289d6719a360786d23a6a3260f6c13f
SolntsevDaniil <zapasnoymail1@yandex.ru>
2018-03-19 17:40:12 +0300

    Final version with comments

---
+++ script.js
@@ -0,0 +1,39 @@
+/* ---------- Start of the program ---------- */
+
+
+// Declaration of strict mode
+'use strict';
+
+// Page load ending event
+window.addEventListener('DOMContentLoaded', function() {
+
+	// Submit event
+	document.getElementById('submit').addEventListener('click', function() {
+
+		// Textarea searching
+		var textArea = document.getElementById('input'),
+			// Textarea value is name of song
+			songName = textArea.value;
+
+		if (!songName || songName == '') {
+			alert('Please, enter the name of the song');
+		} else {
+
+			// Creating of new element
+			var li = document.createElement('li'),
+				// List searching
+				ul = document.getElementById('playlist');
+
+			// Adding song name to the page
+			li.innerHTML = songName;
+			li.className = 'playlist__item';
+			ul.appendChild(li);
+
+			// Value clearing
+			textArea.value = '';
+		};
+	});
+});
+
+
+/* ---------- End of the program ---------- */
